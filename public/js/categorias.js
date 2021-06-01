eb4ad8863bfc2f5b08753635b203b441038d9e28
MatitodhPaker <hueco35.perez@gmail.com>
2021-06-01 02:15:15 -0500

    se agragan procesos

--- public/js/categorias.js
+++ public/js/categorias.js
@@ -13,6 +13,7 @@ function agregarcategoria() {
       respuesta = respuesta.trim();
       if (respuesta == 1) {
         $('#frmAgregarcategoria')[0].reset();
+        $('#cargartablacategorias').load('vistas/categorias/tablacategorias.php');
         swal(":D","Se agrego con exito","success");
       }else{
         swal(":(","No se pudo agregar","error");
@@ -20,17 +21,30 @@ function agregarcategoria() {
     }
   });
 }
-function eliminarcategoria() {
-    swal({
-      title: "¿Estas seguro que quieres eliminar esta categoria?",
-      text: "Una vez eliminado no podra sera recuperado!",
-      icon: "warning",
-      buttons: true,
-      dangerMode: true,
-    })
-    .then((willDelete) => {
-      if (willDelete) {
-        swal("Se ha eliminado");
-      } 
-    });
+function eliminarcategoria(idCategoria) {
+  swal({
+    title: "¿Estas seguro que quieres eliminar esta categoria?",
+    text: "Una vez eliminado no podra sera recuperado!",
+    icon: "warning",
+    buttons: true,
+    dangerMode: true,
+  })
+  .then((willDelete) => {
+    if (willDelete) {
+      $.ajax({
+				type: "POST",
+				data: "idCategoria=" + idCategoria,
+				url: "procesos/categorias/eliminarcategoria.php",
+				success:function(respuesta) {
+					respuesta = respuesta.trim();
+					if (respuesta == 1) {
+						$('#cargartablacategorias').load('vistas/categorias/tablacategorias.php');
+						swal(":D","Se elimino con exito!","success");
+					} else {
+						swal(":(","No se pudo eliminar!","error");
+					}
+				}
+			});
+    } 
+  });
 }
