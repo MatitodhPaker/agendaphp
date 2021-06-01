a90a3f1958a56c430167862a0baaf961df30e932
MatitodhPaker <hueco35.perez@gmail.com>
2021-05-31 22:06:01 -0500

    se modifica los archivos de las tablas

--- vistas/categorias/tablacategorias.php
+++ vistas/categorias/tablacategorias.php
@@ -1,3 +1,33 @@
-<h1>holamundo</h1>
-<span class="btn btn-success" data-toggle="modal" data-target="#actualizarCategoria">editar</span>
-<span class="btn btn-danger" onclick="eliminarcategoria()">eliminar</span>
+<div class="card">
+  <div class="card-body">
+    <div class="table-responsive">
+      <table class="table table-hover table-condensed" id="tablaCategoriasDt">
+        <thead>
+          <th>Nombre</th>
+          <th>Descripcion</th>
+          <th>Editar</th>
+          <th>Eliminar</th>
+        </thead>
+        <tbody>
+          <td></td>
+          <td></td>
+          <td>
+            <span class="btn btn-warning btn-sm" data-toggle="modal" data-target="#actualizarCategoria">
+              <i class="fas fa-edit"></i>
+            </span>
+          </td>
+          <td>
+            <span class="btn btn-danger btn-sm" onclick="eliminarcategoria()">
+              <i class="far fa-trash-alt"></i>
+            </span>
+          </td>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
+<script>
+  $(document).ready(function () {
+    $('#tablaCategoriasDt').DataTable();
+  });
+</script>
--- vistas/contactos/tablacontactos.php
+++ vistas/contactos/tablacontactos.php
@@ -1,7 +1,7 @@
 <div class="card">
   <div class="card-body">
     <div class="table-responsive">
-      <table class="table table-hover table-condensed">
+      <table class="table table-hover table-condensed" id="tablaContactosDt">
         <thead>
           <th>Apellido paterno</th>
           <th>Apellido materno</th>
@@ -20,11 +20,22 @@
           <td></td>
           <td></td>
           <td>
+            <span class="btn btn-warning btn-sm" data-toggle="modal" data-target="#actualizarContacto">
+              <i class="fas fa-edit"></i>
+            </span>
           </td>
           <td>
+            <span class="btn btn-danger btn-sm" onclick="eliminarcontacto()">
+              <i class="far fa-trash-alt"></i>
+            </span>
           </td>
         </tbody>
       </table>
     </div>
   </div>
 </div>
+<script>
+  $(document).ready(function () {
+    $('#tablaContactosDt').DataTable();
+  });
+</script>
